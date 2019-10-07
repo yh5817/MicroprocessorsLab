57f664d76d8c8b9d3f8f37920d67cbaa5683be26
yh5817 <yh5817@skbl-422-002.ph.ic.ac.uk>
2019-10-07 14:32:14 +0100

    using port D as input

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,18 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	; ******* Port D Setup Code ****
+	movlw 0xff ; all bits in
+	movwf TRISD, A ; Port D Direction Register
+	bsf PADCFG1,RDPU, A ; Turn on pull-ups for Port D
+	
 	movlw 	0x0
 	movwf	TRISE, ACCESS	    ; Port C all outputs
 	bra 	test
 loop	movff 	0x06, PORTE
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0xFF
+	movf    PORTE,W,ACCESS
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
