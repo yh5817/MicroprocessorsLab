a62941c4d3b9d58427451cdba5169e2880087e1e
yh5817 <yh5817@skbl-422-002.ph.ic.ac.uk>
2019-10-21 17:11:57 +0100

    First Step

--- Simple1.asm
+++ Simple1.asm
@@ -1,8 +1,33 @@
 	#include p18f87k22.inc
-
+        
+	code
+	org 0x0
+	goto start 
+	org 0x100
 start
-	movlw 0x00
+	movlw 0x0f
 	movwf TRISE, ACCESS
-	bsf PADCFG1, REPU, banked
+	banksel PADCFG1
+	bsf PADCFG1, REPU, BANKED ; Turn on pull-ups for port E
+	
+	clrf LATE  ; Write 0s to the LATE register
+	movlw 0x0f
+	movwf LATE, ACCESS
+	
+	movlw high(0xff)
+	movwf 0x10
+	movlw low (0xff)
+	movwf 0x11
+	call bigdelay	
+	
+	goto   $
 
+bigdelay
+	movlw 0x00
+dloop   decf 0x11, f
+	subwfb 0x10, f
+	bc dloop 
+	return
+	
 	end
+	
